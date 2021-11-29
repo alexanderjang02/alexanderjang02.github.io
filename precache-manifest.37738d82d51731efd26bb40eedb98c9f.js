8b7f68f92d919131fbd3085925a15ded28a9af1e
Alexander Jang <atjang@berkeley.edu>
2021-11-28 19:08:12 -0800

    Updates

---
+++ precache-manifest.37738d82d51731efd26bb40eedb98c9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7b397a9ced42b3eb47e9e8f58f40b2",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "15f31ac3a749bca5abbb",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/home/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f31ac3a749bca5abbb",
+    "url": "/home/static/js/main.5bc02709.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
